e7692af8b7e984e33f215759970621a9ef06f40a
Alvie Rahman <alvierahman90@gmail.com>
2021-04-06 13:46:18 +0100

    revert to commit 3ca1fbf

--- config.def.h
+++ config.def.h
@@ -97,24 +97,24 @@ unsigned int tabspaces = 8;
 static const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#fbf1c7", /* hard contrast: #1d2021 / soft contrast: #32302f */
+  [0] = "#32302f", /* hard contrast: #1d2021 / soft contrast: #32302f */
   [1] = "#cc241d", /* red     */
   [2] = "#98971a", /* green   */
   [3] = "#d79921", /* yellow  */
   [4] = "#458588", /* blue    */
   [5] = "#b16286", /* magenta */
   [6] = "#689d6a", /* cyan    */
-  [7] = "#ebdbb2", /* white   */
+  [7] = "#a89984", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#928374", /* gray   */
+  [8]  = "#928374", /* black   */
   [9]  = "#fb4934", /* red     */
   [10] = "#b8bb26", /* green   */
   [11] = "#fabd2f", /* yellow  */
   [12] = "#83a598", /* blue    */
   [13] = "#d3869b", /* magenta */
   [14] = "#8ec07c", /* cyan    */
-  [15] = "#3c3836", /* white   */
+  [15] = "#ebdbb2", /* white   */
 };
 
 /*
